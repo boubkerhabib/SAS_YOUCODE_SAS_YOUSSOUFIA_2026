c6f65dd6284cd57bfd1e50f573443cec237e5839
boubkerhabib <boubkerhabib41@gmail.com>
2026-09-01 09:54:31 +0100

    Initial commit

---
+++ DAY1/variable/Challenge2.js
@@ -0,0 +1,5 @@
+//Challenge 2 : Conversion de monnaie
+let montantMAD;
+let montantEUR = 200;
+montantMAD = montantEUR*11;
+console.log("le montant en EUR: " + montantEUR + "\nchanger a mantant MAD: " + montantMAD)
---
+++ DAY1/variable/Challenge3.js
@@ -0,0 +1,6 @@
+//Challenge 3 : Conversion de stockage
+let Gigaoctets = 5;
+let Megaoctets;
+Megaoctets = Gigaoctets*1024;
+console.log("Stockage : " + Gigaoctets + "GB") 
+console.log("Résultat : " + Megaoctets + "MB")
