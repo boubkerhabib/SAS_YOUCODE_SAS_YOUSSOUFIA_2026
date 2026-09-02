3c9685513064bb61e3bfa8fda091099299913da1
boubkerhabib <boubkerhabib41@gmail.com>
2026-09-02 01:38:25 +0100

    changer readline a prompt-sync parce que prompt-sync facil

--- DAY1/condition1/parte1/Challange1.js
+++ DAY1/condition1/parte1/Challange1.js
@@ -1,8 +1,5 @@
 //Challenge 1 : Vérifier l'âge
 let prompt = require('prompt-sync')();
-//
-// get input from the user.
-//
 let age = prompt('entrez votre age : ');
 n = Number(age);
 if (age >= 18){
